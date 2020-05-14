352c0aed81b3feeeacb2a41ea8cd32bec61df6b7
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-14 16:35:55 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,24 +1,24 @@
 function basicTeenager(age) {
   if (age >= 13 && age <= 19) {
-    return("You are a teenager!")
+    return ("You are a teenager!")
   }
 }
 
 function teenager(age) {
   if (age >= 13 && age <= 19) {
-    return("You are a teenager!")
+    return ("You are a teenager!")
   } else {
-    return("You are not a teenager")
+    return ("You are not a teenager")
   }
 }
 
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
-    return("You are a teenager!")
+    return ("You are a teenager!")
   } else if (age < 12){
-    return("You are a kid")
+    return ("You are a kid")
   } else if (age > 19) {
-    return("You are a grownup")
+    return ("You are a grownup")
   }
 }
 
@@ -28,19 +28,19 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   case 13:
-    return("You are a teenager");
+    return ("You are a teenager")
   case 14:
-    return("You are a teenager");
+    return ("You are a teenager")
   case 15:
-    return("You are a teenager");
+    return ("You are a teenager")
   case 16:
-    return("You are a teenager");
+    return ("You are a teenager")
   case 17:
-    return("You are a teenager");
+    return ("You are a teenager")
   case 18:
-    return("You are a teenager");
+    return ("You are a teenager")
   case 19:
-    return("You are a teenager");
+    return ("You are a teenager")
   default:
-    return("You have an age")
+    return ("You have an age")
 }
