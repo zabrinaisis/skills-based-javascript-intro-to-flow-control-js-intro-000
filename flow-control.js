ddfa3a3276355b03eb40b77a13a06cb148131057
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-14 16:32:55 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -23,9 +23,24 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-
+  return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-
+  case 13:
+    return("You are a teenager");
+  case 14:
+    return("You are a teenager");
+  case 15:
+    return("You are a teenager");
+  case 16:
+    return("You are a teenager");
+  case 17:
+    return("You are a teenager");
+  case 18:
+    return("You are a teenager");
+  case 19:
+    return("You are a teenager");
+  default:
+    return("You have an age")
 }
